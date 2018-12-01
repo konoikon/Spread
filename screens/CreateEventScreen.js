dc10f758acb11bd450b139c8c2f7aa964cebe24e
Konstantinos Oikonomou <amerikanoc@gmail.com>
2018-12-01 17:19:08 -0600

    Firebase Integration Installed

--- screens/CreateEventScreen.js
+++ screens/CreateEventScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, ScrollView, Button, Alert,
          TextInput} from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 const styles = StyleSheet.create({
   label: {
@@ -10,19 +12,50 @@ const styles = StyleSheet.create({
   },
 });
 
+const config = {
+    apiKey: "AIzaSyBsr9S-GTboxVbz_Xi5VFvXDHjRahwozWc",
+    authDomain: "spread-7fdc3.firebaseapp.com",
+    databaseURL: "https://spread-7fdc3.firebaseio.com",
+    projectId: "spread-7fdc3",
+    storageBucket: "spread-7fdc3.appspot.com",
+    messagingSenderId: "1091492798848"
+  };
+firebase.initializeApp(config);
+
 export default class CreateEventScreen extends Component {
 
   constructor(props){
     super(props)
     this.state = {eventTitle: "title",
                   eventDescription: "description",
+                  eventLocation: "loc",
                   eventDatetime: new Date(),
                   eventInvite1Name: "invite1Name",
                   eventInvite1Phone: "invite1Phone",
                   eventInvite2Name: "invite2Name",
                   eventInvite2Phone: "invite2Name",}
+
+    this.createEvent = this.createEvent.bind(this);
   }
 
+
+  createEvent() {
+    if (!this.state.eventTitle) return;
+
+    firebase.firestore().settings( { timestampsInSnapshots: true });
+
+    const event = firebase.firestore().collection('events')
+                  .add({title: this.state.eventTitle,
+                        description: this.state.eventDescription,
+                        location: this.state.eventLocation,
+                        datetime: this.state.eventDatetime,
+                        eventInvite1Name: this.state.eventInvite1Name,
+                        eventInvite2Name: this.state.eventInvite2Name,
+                        eventInvite1Phone: this.state.eventInvite1Phone,
+                        eventInvite2Phone: this.state.eventInvite1Phone});
+
+    this.props.navigation.navigate('Home');
+  }
 
   render() {
     return(
@@ -39,6 +72,12 @@ export default class CreateEventScreen extends Component {
             style={{height: 40}}
             placeholder="Event Description"
             onChangeText={(eventDescription) => this.setState({eventDescription})}
+          />
+          <Text style={styles.label}>Location</Text>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Event Location"
+            onChangeText={(eventLocation) => this.setState({eventLocation})}
           />
         </View>
         <View style={{backgroundColor: 'red', flex: 1, paddingLeft: 10}}>
@@ -81,9 +120,7 @@ export default class CreateEventScreen extends Component {
         <View style={{backgroundColor: 'white', flex:0.4}}>
           <Button
             title="Create Event"
-            onPress={() => {
-              Alert.alert("Create Event Button Pressed");
-            }}
+            onPress={this.createEvent}
           />
         </View>
       </View>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ScrollView, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Button, Alert,
+         FlatList} from 'react-native';
 import Event from '../components/Event.js';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 export default class HomeScreen extends Component {
+
+  constructor(props) {
+    super(props)
+    firebase.firestore().settings( { timestampsInSnapshots: true });
+    this.ref = firebase.firestore().collection('events');
+    this.unsubscribe = null;
+
+    this.state = {loading: true,
+                  events: [],}
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const events = [];
+
+    querySnapshot.forEach((doc) => {
+      const {title, datetime, location, description,
+              eventInvite1Phone, eventInvite2Name, eventInvite1Name,
+              eventInvite2Phone,} = doc.data();
+
+      events.push({
+        title,
+        datetime,
+        location,
+      });
+    });
+
+    this.setState({
+      events,
+      loading: false,
+    });
+  }
 
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View style={{flex:1,}}>
         <ScrollView style={{flex:1, padding:20, backgroundColor:'skyblue',}}>
-          <Event title='Event Title' datetime='Datetime' location='Location'/>
-          <Event title='Event Title' datetime='Datetime' location='Location'/>
+          <FlatList
+            data={this.state.events}
+            renderItem={({item}) => <Event {...item}/>}
+          />
         </ScrollView>
         <View style={{width:'auto',height:200, backgroundColor:'red', alignItems: 'center'}}>
           <Button
